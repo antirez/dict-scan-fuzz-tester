68951c8dce682893bdaaf42766c8723f3eb20bde
antirez <antirez@gmail.com>
2018-06-01 13:58:59 +0200

    Expansion of visited buckets.

--- scan.c
+++ scan.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 typedef struct {
     unsigned long size;
@@ -72,35 +74,73 @@ unsigned long dictScan(dictht *t0, dictht *t1,
     return v;
 }
 
-void check(dictht *t, char *name) {
+int check(dictht *t, char *name) {
     unsigned long i;
+    int errors = 0;
     printf("Checking table %s of size %lu\n", name, t->size);
     for (i = 0; i < t->size; i++) {
-        if (t->visited[i] == 0)
+        if (t->visited[i] == 0) {
             printf("Bucket %lu not visited!\n",i);
+            errors++;
+        }
+    }
+    return errors;
+}
+
+/* When a new hash table is added, we need to mark as visited all
+ * the buckets in the new table that, in theory, do not require to be
+ * vesited. When the new table is bigger, those are the expansion of
+ * the old buckets to the new table. When the new table is instead smaller
+ * we can mark only the buckets that have their expansion in the old
+ * (bigger) table, all already marked as visited. */
+void expand(dictht *old, dictht *new) {
+    if (old->size <= new->size) {
+        for (unsigned long i = 0; i < old->size; i++) {
+            if (old->visited[i]) {
+                for (unsigned long j = i; j < new->size; j += old->size) {
+                    new->visited[j] = 1;
+                }
+            }
+        }
+    } else {
+        for (unsigned long i = 0; i < new->size; i++) {
+            unsigned long j;
+            for (j = i; j < old->size; j += new->size) {
+                if (!old->visited[j]) break;
+            }
+            if (j >= old->size) {
+                /* All the expansions are already visited in the old
+                 * table, so we can mark the new table bucket as visited. */
+                new->visited[i] = 1;
+            }
+        }
     }
 }
 
-void expand(dictht *old, dictht *new) {
-}
-
 int main(void) {
+    srand(time(NULL));
     dictht t0, t1;
 
-    t1.size = 8;
-    t1.sizemask = t1.size-1;
-    memset(t1.visited,0,sizeof(t1.visited));
-
-    t0.size = 32;
+    t0.size = 8;
     t0.sizemask = t0.size-1;
     memset(t0.visited,0,sizeof(t0.visited));
 
+    t1.size = 32;
+    t1.sizemask = t1.size-1;
+    memset(t1.visited,0,sizeof(t1.visited));
+
     unsigned long cursor = 0;
     int iteration = 0;
+    int before_rehashing = 1;
     int first_rehashing_step = 1;
     do {
-        if (iteration < 1) {
-            cursor = dictScan(&t1, NULL, cursor, NULL);
+        if (before_rehashing) {
+            cursor = dictScan(&t0, NULL, cursor, NULL);
+            if (!(rand() % t0.size)) {
+                before_rehashing = 0;
+                printf("Rehashing to new table: %lu -> %lu\n",
+                    t0.size, t1.size);
+            }
         } else {
             if (first_rehashing_step) {
                 expand(&t0,&t1);
@@ -113,8 +153,11 @@ int main(void) {
     } while (cursor != 0);
 
     /* Check that the two tables were visited. */
-    check(&t0,"table 0");
-    check(&t1,"table 1");
+    if (check(&t0,"table 0")) exit(1);
+    /* Check the second talbe only if the rehashing happened. */
+    if (!first_rehashing_step) {
+        if (check(&t1,"table 1")) exit(1);
+    }
 
     return 0;
 }
